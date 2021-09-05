ca097f00f525752ce03d2a5fada8bb8675f15f42
JoyceQuerubino <joycequerubino5@gmail.com>
2021-09-05 20:13:25 -0300

    Add fonts personlizadas e desidade de Pixel

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,31 @@
 import React from "react";
 
-import { Container, Title } from './styles'; 
+import { 
+    Container,
+    UserContainer,
+    Header,
+    UserInfo,
+    Photo,
+    User, 
+    UserGreeting, 
+    UserName,
+} from './styles'; 
 
 export function Dashboard(){
     return(
         <Container>
-            <Title>Dashboard</Title>
+            <Header>
+                <UserContainer>
+                    <UserInfo>
+                        <Photo source={{ uri: 'https://avatars.githubusercontent.com/u/66806696?v=4' }} />
+                        <User>
+                            <UserGreeting>Olá,</UserGreeting>
+                            <UserName>Joyce</UserName>
+                        </User>
+                    </UserInfo>
+                </UserContainer>
+
+            </Header>
         </Container>
     )
 }
