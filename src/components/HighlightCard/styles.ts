84cc254829f4719e50cca1dc8f3d79d0954c9e59
JoyceQuerubino <joycequerubino5@gmail.com>
2021-09-06 18:51:50 -0300

    Add propriedades e estilizando cartoes

--- src/components/HighlightCard/styles.ts
+++ src/components/HighlightCard/styles.ts
@@ -1,9 +1,20 @@
-import styled from 'styled-components/native'; 
+import styled, {css} from 'styled-components/native'; 
 import { Feather } from '@expo/vector-icons'; 
 import { RFValue } from 'react-native-responsive-fontsize';
 
-export const Container = styled.View `
-    background-color: ${({ theme }) => theme.colors.shape};
+
+// Tipagens
+//Criar uma tipagem para conseguir declarar as cores de cada tipo de icone
+interface CardsProps {
+    type: 'up' | 'down' | 'total';
+};
+
+
+//Estilizações
+export const Container = styled.View<CardsProps>`
+    background-color: ${({ theme, type }) => 
+    type === "total" ? theme.colors.secondary : theme.colors.shape};
+
     width: ${RFValue(300)}px;
     border-radius: 5px;
     padding: 19px 23px;
@@ -16,27 +27,46 @@ export const Header = styled.View `
   justify-content: space-between;
 `;
 
-export const Title = styled.Text `
+export const Title = styled.Text<CardsProps>`
+    color: ${({ theme, type }) => 
+    type === "total" ? theme.colors.shape : theme.colors.text_dark};
+
     font-family: ${({ theme }) => theme.fonts.regular};
-    color: ${({ theme }) => theme.colors.text_dark};
     font-size: ${RFValue(14)}px;
 `;
 
-export const Icon = styled(Feather)`
+//Esse ícone vai receber a tipagem da interface IconProps
+export const Icon = styled(Feather)<CardsProps>`
     font-size: ${RFValue(40)}px;
+    
+    ${(props) => props.type === 'up' && css`
+        color: ${({ theme }) => theme.colors.sucess};
+    `}
+
+    ${(props) => props.type === 'down' && css`
+        color: ${({ theme }) => theme.colors.attention};
+    `}
+
+    ${(props) => props.type === 'total' && css`
+        color: ${({ theme }) => theme.colors.shape};
+    `}
 `;
 
 export const Footer = styled.View ``; 
 
-export const Amount = styled.Text `
+export const Amount = styled.Text<CardsProps>`
+    color: ${({ theme, type }) => 
+    type === "total" ? theme.colors.shape : theme.colors.text_dark};
+
     font-family: ${({ theme }) => theme.fonts.medium};
-    color: ${({ theme }) => theme.colors.text_dark};
     font-size: ${RFValue(32)}px;
     margin-top: 38px;
 ` ;
 
-export const LastTransaction  = styled.Text `
+export const LastTransaction  = styled.Text<CardsProps>`
+    color: ${({ theme, type }) => 
+    type === "total" ? theme.colors.shape : theme.colors.text};
+    
     font-family: ${({ theme }) => theme.fonts.regular};
-    color: ${({ theme }) => theme.colors.text};
     font-size: ${RFValue(12)}px;
 `; 
